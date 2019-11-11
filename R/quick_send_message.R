e1d14726d645752aeda8298fe7abab6cfd084978
tmakowski <tymek.makowski@gmail.com>
2019-11-11 15:23:07 +0100

    Removed available_clients function. Moved a functionality of sending quick messages to separate function (to be developed).

--- R/quick_send_message.R
+++ R/quick_send_message.R
@@ -1,12 +1,14 @@
-#' @title Available clients
-#' 
-#' @description TODO
-#' 
-#' @rdname available_clients
-#' 
-#' @export
-available_clients <- function() {
-    names(clients_dict())
+# TODO(TM):
+quick_send_message <- function(client, message, destination, verbose = FALSE,
+                               ...) {
+    assert(is.character(client) || is.client_notifieR(client),
+           "only character and notifieR client's are supported") # TODO(TM)
+
+    if (is.character(client)) {
+        client <- create_client(service_name = client, ...)
+    }
+
+    return(send_message(client, message, destination, verbose, ...))
 }
 
 
@@ -16,29 +18,30 @@ create_client <- function(service_name, ...) {
     service <- clients_dict()[[tolower(service_name)]]
     assert(!is.null(service), "could not create client:",
            "client for service", service_name, "is not implemented.",
-           "See ?available_clients")
-    
-    # TODO: reading from environment
+           "See ?available_clients") # TODO(TM): update assert
+
+    # TODO(Feature): reading arguments from a system environment.
     arguments <- list(...)
     defaults <- service$default_fields(NULL)
     assert(all(defaults %in% names(arguments)), "could not create a ",
            service_name, "client: insufficient arguments provided.",
            sprintf("See ?client_%s for details.", service_name))
-    
+
     do.call(service$constructor, arguments[defaults])
 }
 
 
-# A list-based dictionary translating a service name to a corresponding client
-# constructor.
+# A list-based dictionary translating a service name to a corresponding client.
+# Note: Only clients viable for quick_send_message (not requiring authorization)
+#   are stored in the dict.
 clients_dict <- function() {
     list(
         "notifier" = list(
             "constructor" = client_notifieR,
             "default_fields" = default_fields.client_notifieR),
-        "gmail" = list(
-            "constructor" = client_gmail,
-            "default_fields" = default_fields.client_gmail),
+        "slack" = list(
+            "constructor" = client_slack,
+            "default_fields" = default_fields.client_slack),
         "telegram" = list(
             "constructor" = client_telegram,
             "default_fields" = default_fields.client_telegram)
--- R/send_message.R
+++ R/send_message.R
@@ -12,14 +12,10 @@
 #' 
 #' @export
 send_message <- function(client, message, destination, verbose = FALSE, ...) {
-    if (is.character(client)) {
-        client <- create_client(service_name = client, ...)
-        return(send_message(client, message, destination, verbose, ...))
-    }
-    
     assert(is.client_notifieR(client), "could not execute send_method:",
            not_a_client("client", "notifieR"))
     
-    # TODO: message & destination assertions
+    # TODO(TM): assert message argument
+
     UseMethod("send_message")
 }
